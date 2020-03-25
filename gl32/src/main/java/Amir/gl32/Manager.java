4c3adaccea5264edf1b4e9df1cbf93e590bfa5ee
Amir <amir.benmallem@gmail.com>
2020-03-25 16:56:23 +0100

    manager

---
+++ gl32/src/main/java/Amir/gl32/Manager.java
@@ -0,0 +1,10 @@
+package Amir.gl32;
+
+public class Manager  extends Employe {
+	private double nombre_subordonnée;
+	public Manager(double nombre_subordonnée) {
+		super(nom,prenom,annee_debut);
+		this.nombre_subordonnée = nombre_subordonnée;
+	}
+
+}
